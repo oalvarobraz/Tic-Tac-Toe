e9d3800b8e1533709df5e01b9c29ff7cb994e3ea
oalvarobraz <114008085+oalvarobraz@users.noreply.github.com>
2023-03-25 21:19:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import game
 
-keep = 1
-while keep:
-    game.start_game()
-    keep = int(input("|| Digite 1 caso queria continuar jogando ou 0 caso queira sair: "))
+if __name__ == '__main__':
+    print("Welcome to the Hash game!")
+    keep_playing = True
 
+    while keep_playing:
+        game.start_game()
+        play_again = input("Do you want to play again? (S/N): ").strip().lower()
+        while play_again not in ('s', 'n'):
+            play_again = input("Type S to continue or N to get out: ").strip().lower()
+        keep_playing = play_again == 's'
+    print("Thanks for playing!")
